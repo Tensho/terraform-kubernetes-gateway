cdf342520afb82c948ee45e6d348185fa5e0edbc
Andrew Babichev <andrew.babichev@gmail.com>
2025-09-19 17:18:35 +0100

    style: delete kubernetes_annotations reference

--- main.tf
+++ main.tf
@@ -33,21 +33,6 @@ resource "kubernetes_manifest" "default" {
   }
 }
 
-# Doesn't work as Gateway resource requires "spec" field on PATCH action
-# resource "kubernetes_annotations" "default" {
-#   api_version = kubernetes_manifest.default.manifest.apiVersion
-#   kind        = kubernetes_manifest.default.manifest.kind
-#
-#   metadata {
-#     namespace = kubernetes_manifest.default.manifest.metadata.namespace
-#     name      = kubernetes_manifest.default.manifest.metadata.name
-#   }
-#
-#   annotations = {
-#     "foo" = "bar"
-#   }
-# }
-
 resource "kubernetes_manifest" "gcp_gateway_policy" {
   count = var.ssl_policy != null ? 1 : 0
 
